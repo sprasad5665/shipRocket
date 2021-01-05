c2a700b19796407cae3a602eb0f7f2e60223f5b8
saiprasad <sprasad5665@gmail.com>
2021-01-05 17:07:52 +0530

    first commit

---
+++ src/test/java/com/example/shipRocket/ShipRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shipRocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
